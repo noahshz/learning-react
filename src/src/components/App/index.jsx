fb8f8f3120fc8d5c476c39e3519e4a5533cbcd65
Noah <borkitube@gmail.com>
2022-09-20 11:23:41 +0200

    structure

---
+++ src/src/components/App/index.jsx
@@ -0,0 +1,12 @@
+import Product from '../Product'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Product/>
+    </div>
+  );
+}
+
+export default App;
